a92a775e874b05e7df8abd9947350e2d612d72aa
Mark Ross <krazkidd@gmail.com>
2023-12-25 21:56:42 -0700

    Show draft content items in DEV mode.

--- src/lib/contentParser.ts
+++ src/lib/contentParser.ts
@@ -3,15 +3,12 @@ import type { CollectionEntry } from 'astro:content';
 
 import { slugify } from "./utils/textConverter";
 
-//TODO allow drafts in DEV mode
-//     https://docs.astro.build/en/guides/content-collections/#filtering-collection-queries
-
 export async function getAllAuthors() {
-  return getCollection('authors', ({ data }) => !data.draft);
+  return getCollection('authors', ({ data }) => !(import.meta.env.PROD && data.draft));
 };
 
 export async function getAllPosts() {
-  return getCollection('posts', ({ data }) => !data.draft);
+  return getCollection('posts', ({ data }) => !(import.meta.env.PROD && data.draft));
 };
 
 export async function getAuthorsByPost(post: CollectionEntry<'posts'>) {
@@ -19,11 +16,11 @@ export async function getAuthorsByPost(post: CollectionEntry<'posts'>) {
 };
 
 export async function getAllPostsByAuthor(author: CollectionEntry<'authors'>) {
-  return getCollection('posts', ({ data }) => !data.draft && data.authors.map(author => author.slug).includes(author.slug));
+  return getCollection('posts', ({ data }) => !(import.meta.env.PROD && data.draft) && data.authors.map(author => author.slug).includes(author.slug));
 };
 
 export async function getFeaturedPosts() {
-  return getCollection('posts', ({ data }) => !data.draft && data.featured);
+  return getCollection('posts', ({ data }) => !(import.meta.env.PROD && data.draft) && data.featured);
 };
 
 export async function getAllPostCategories() {
@@ -47,9 +44,9 @@ export async function getAllPostTags() {
 };
 
 export async function getAllServices() {
-  return getCollection('services', ({ data }) => !data.draft);
+  return getCollection('services', ({ data }) => !(import.meta.env.PROD && data.draft));
 };
 
 export async function getAllPortfolioProjects() {
-  return getCollection('portfolio', ({ data }) => !data.draft);
+  return getCollection('portfolio', ({ data }) => !(import.meta.env.PROD && data.draft));
 };
